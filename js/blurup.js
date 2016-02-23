70f9b1fb6fae639482d3811ce7b9216d53e5886b
Amanda Saffer <asaffer@sosorgans.org>
2016-02-22 19:37:48 -0700

    Add blurup to main landing background

--- js/blurup.js
+++ js/blurup.js
@@ -1,6 +1,6 @@
 function preload(ele, imgName) {
   var c = new Image(); // create img element for preload
-  var ele = '.-' + ele; // add hyphen to beginning of class name
+  var ele = '.' + ele; // add hyphen to beginning of class name
   
   // when the image element is loaded, set the background image
   c.onload = function(){
@@ -12,13 +12,14 @@ function preload(ele, imgName) {
 }
 
 $(document).ready(function() {
-  preload('spacedojo', 'josh.jpg');
-  preload('sos', 'laura.jpg');
-  preload('tbb', 'couch.jpg');
-  preload('wizard-school', 'card-bg.jpg');
-  preload('betkong', 'esports.jpg');
-  preload('woofgang', 'sonya.jpg');
-  preload('testimonials', 'head.jpg');
+  preload('-spacedojo', 'josh.jpg');
+  preload('-sos', 'laura.jpg');
+  preload('-tbb', 'couch.jpg');
+  preload('-wizard-school', 'card-bg.jpg');
+  preload('-betkong', 'esports.jpg');
+  preload('-woofgang', 'sonya.jpg');
+  preload('-testimonials', 'head.jpg');
+  preload('landing', 'head.jpg');
 });
 
 document.querySelector( "#site-menu" ).addEventListener( "click", function() {
