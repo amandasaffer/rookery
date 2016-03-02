2ba1363960f7dce6a6ad07a1c27e79989a3844a6
Amanda Saffer <amandasaffer@gmail.com>
2016-03-02 12:55:24 -0700

    work in progress

--- js/blurup.js
+++ js/blurup.js
@@ -19,7 +19,27 @@ $(document).ready(function() {
   preload('-betkong', 'esports.jpg');
   preload('-woofgang', 'sonya.jpg');
   preload('-testimonials', 'head.jpg');
-  preload('landing', 'head.jpg');
+  // preload('landing', 'head.jpg');
+
+  $('.featured-cases').slick({
+    // define custom arrows
+    prevArrow: '<div class="arrow-link"><div class="arrow left-arrow"><svg class="icon arrow left-arrow"><use xlink:href="#left-arrow"></use></svg></div></div>',
+    nextArrow: '<div class="arrow-link"><div class="arrow right-arrow"><svg class="icon arrow right-arrow"><use xlink:href="#right-arrow"></use></svg></div></div>',
+    fade: true,
+    speed: 1000, // fade speed
+    autoplay: true,
+    autoplaySpeed: 5000 // 5 second on each slide
+  });
+
+  $('.testimonial-box').slick({
+    arrows: false,
+    dots: true,
+    fade: true,
+    speed: 1000, // fade speed
+    autoplay: true,
+    autoplaySpeed: 10000, // 10 second on each slide
+    pauseOnHover: false
+  });
 });
 
 document.querySelector( "#site-menu" ).addEventListener( "click", function() {
