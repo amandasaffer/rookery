ff5ad657628526d0212576e15a665766b26cb92c
Amanda Saffer <amandasaffer@gmail.com>
2016-08-20 18:37:25 -0600

    first major update to light theme

--- js/blurup.js
+++ js/blurup.js
@@ -1,7 +1,7 @@
 function preload(ele, imgName) {
   var c = new Image(); // create img element for preload
   var ele = '.' + ele; // add hyphen to beginning of class name
-  
+
   // when the image element is loaded, set the background image
   c.onload = function(){
     $(ele).css('background-image','url(/img/' + imgName + ')');
@@ -15,47 +15,9 @@ $(document).ready(function() {
   preload('-spacedojo', 'josh.jpg');
   preload('-sos', 'laura.jpg');
   preload('-tbb', 'couch.jpg');
-  preload('-wizard-school', 'card-bg.jpg');
   preload('-betkong', 'esports.jpg');
   preload('-woofgang', 'sonya.jpg');
   preload('-testimonials', 'head.jpg');
-  preload('landing', 'woods1.jpg');
-
-
-
-  // ------------------
-  // CASES SLIDER
-  $('.featured-cases').slick({
-
-    // define custom arrows
-    prevArrow: '<div class="arrow-link"><div class="arrow left-arrow"><svg class="icon"><use xlink:href="#left-arrow"></use></svg></div></div>',
-    nextArrow: '<div class="arrow-link"><div class="arrow right-arrow"><svg class="icon"><use xlink:href="#right-arrow"></use></svg></div></div>',
-    fade: true,
-
-    // fade speed
-    speed: 1000,
-    autoplay: true,
-
-    // 5 second on each slide
-    autoplaySpeed: 5000
-  });
-
-
-  // ------------------
-  // TESTIMONIAL SLIDER
-  $('.testimonial-box').slick({
-    arrows: false,
-    dots: true,
-    fade: true,
-
-    // fade speed
-    speed: 1000,
-    autoplay: true,
-
-    // 10 second on each slide
-    autoplaySpeed: 10000,
-    pauseOnHover: false
-  });
 });
 
 document.querySelector( "#site-menu" ).addEventListener( "click", function() {
